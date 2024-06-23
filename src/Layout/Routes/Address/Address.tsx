81f34f112661d9cd9678871bc61cb93554a2da30
NoOne451 <ranjithdurai111@gmail.com>
2024-06-23 22:44:00 +0530

    sample

--- src/Layout/Routes/Address/Address.tsx
+++ src/Layout/Routes/Address/Address.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 type AddressWithDesc = {
   desc: string;
   phone: number;
@@ -23,45 +23,45 @@ export const Address: React.FC<AddressProps> = ({ address }) => {
   return (
     <div className="px-[20px] py-[10px] flex flex-col justify-start items-start md:flex-row md:items-center md:justify-center capitalize  [&>div]:flex [&>div]:justify-center [&>div]:items-start [&>div]:gap-2 [&>div]:px-[10px] [&>div]:py-[20px]">
       {address.map((addr, index) => {
-        if ("desc" in addr && "phone" in addr) {
+        if ('desc' in addr && 'phone' in addr) {
           return (
             <div key={index}>
               <img
                 className="w-[20px] h-[20px] p-[3px] rounded-[10px] bg-cover bg-brown"
-                src="../src/assets/phone.svg"
+                src="/phone.svg"
                 alt=""
               />
               <div className=" w-custom">
                 <p className="text-[17px] font-[600]">+91 {addr.phone}</p>
-                <p className=" text-gray text-justify">{addr.desc}</p>
+                <p className="text-justify text-gray">{addr.desc}</p>
               </div>
             </div>
           );
-        } else if ("title" in addr && "address" in addr) {
+        } else if ('title' in addr && 'address' in addr) {
           return (
             <div key={index}>
               <img
                 className="w-[20px] h-[20px] p-[3px] rounded-[10px] bg-cover bg-brown"
-                src="../src/assets/location.svg"
+                src="/location.svg"
                 alt=""
               />
               <div>
                 <p className="text-[17px] font-[600]">{addr.title}</p>
-                <p className=" text-gray text-justify">{addr.address}</p>
+                <p className="text-justify text-gray">{addr.address}</p>
               </div>
             </div>
           );
-        } else if ("days" in addr && "time" in addr) {
+        } else if ('days' in addr && 'time' in addr) {
           return (
             <div key={index} className=" md:min-w-48">
               <img
                 className="w-[20px] h-[20px] p-[3px] rounded-[10px] bg-cover bg-brown"
-                src="../src/assets/timer.svg"
+                src="/timer.svg"
                 alt=""
               />
               <div>
                 <p className="text-[17px] font-[600]">{addr.days}</p>
-                <p className=" text-gray text-justify">{addr.time}</p>
+                <p className="text-justify text-gray">{addr.time}</p>
               </div>
             </div>
           );
